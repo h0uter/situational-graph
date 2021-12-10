d9e935cfdc54c5da01e3b6432ef1bc776b56a855
W. Meijer <houterm@gmail.com>
2021-12-10 19:42:41 +0100

    added lattice world

--- world.py
+++ world.py
@@ -2,20 +2,18 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from numpy.core.shape_base import block
 
-class GraphWorld():
+class ManualGraphWorld():
     def __init__(self):
         self.world = nx.Graph()
         self.world.add_node(0, pos=(0, 0))
         self.idx = 1
-
-        # self.create_path_graph_world()
         self.create_complex_graph_world()
 
         self.init_plot()
 
     def create_path_graph_world(self):
         structure = [(4, 0), (8, 0), (12, 0), (16, 0), (16, -4), (16, -8), (16, -12), (12, -12), (12, -8), (12, -4), (12, -1), (8, -1),
-                          (5, 6), (0, 6), (-4, 6), (-8, 6), (-12, 6), (-16, 6), (-16, 11)]
+                     (5, 6), (0, 6), (-4, 6), (-8, 6), (-12, 6), (-16, 6), (-16, 11)]
 
         # TODO:: find a cleaner way to init graph world
         for wp in structure:
@@ -24,7 +22,8 @@ class GraphWorld():
             self.idx += 1
 
     def create_complex_graph_world(self):
-        living = [(12, 0), (16, 0), (16, -4), (16, -8), (16, -12), (12, -12), (12, -8), (12, -4)]
+        living = [(12, 0), (16, 0), (16, -4), (16, -8),
+                  (16, -12), (12, -12), (12, -8), (12, -4)]
         hall = [(4, 0), (8, -1), (5, 6), (0, 6), (-4, 6),
                 (-8, 6), (-12, 6), (-16, 6), (-16, 11)]
         hall_init = False
@@ -39,7 +38,7 @@ class GraphWorld():
                 self.idx += 1
                 hall_init = True
 
-        self.world.add_edge(0, 1) # connect start to the hall
+        self.world.add_edge(0, 1)  # connect start to the hall
 
         for wp in living:
             if living_init:
@@ -50,7 +49,7 @@ class GraphWorld():
                 self.world.add_node(self.idx, pos=wp)
                 self.idx += 1
                 living_init = True
-            
+
         self.world.add_edge(17, 10)
         self.world.add_edge(2, 10)
 
@@ -80,7 +79,6 @@ class GraphWorld():
         self.world.add_edge(self.idx, self.idx-1)
         self.idx += 1
 
-
     def get_node_by_pos(self, pos):
         ''' returns the node at the given position '''
         for node in self.world.nodes():
@@ -105,30 +103,87 @@ class GraphWorld():
 
     def draw_world(self):
         ''' draws the world '''
-        nx.draw_networkx_nodes(self.world, pos=nx.get_node_attributes(self.world, 'pos'),
-                ax=self.ax, node_color='grey')
+        nx.draw_networkx_nodes(
+                                self.world, 
+                                pos=nx.get_node_attributes(self.world, 'pos'),
+                                ax=self.ax, 
+                                node_color='grey')
         nx.draw_networkx_edges(
-            self.world, pos=nx.get_node_attributes(self.world, 'pos'), ax=self.ax, edge_color='grey')
-        nx.draw_networkx_labels(self.world, pos=nx.get_node_attributes(
-            self.world, 'pos'), ax=self.ax, font_size=10)
+                                self.world, 
+                                pos=nx.get_node_attributes(self.world, 'pos'), 
+                                ax=self.ax, 
+                                edge_color='grey')
+
+        nx.draw_networkx_labels(
+                                self.world, 
+                                pos=nx.get_node_attributes(self.world, 'pos'), 
+                                ax=self.ax, 
+                                font_size=10)
+
+        plt.axis('on')
+        self.ax.tick_params(left=True, 
+                            bottom=True,
+                            labelleft=True, 
+                            labelbottom=True)
+        plt.show()
+
+
+# TODO: create abstract world class which implements the plot methods
+class LatticeWorld():
+    def __init__(self):
+        N = 10
+        M = 10
+        self.world = nx.generators.lattice.grid_2d_graph(N, M, periodic=False)
+        for node in self.world.nodes():
+            self.world.nodes[node]["pos"] = (node[0], node[1])
+
+        self.init_plot()
+
+    def init_plot(self):
+        ''' initializes the plot'''
+        # plt.ion()
+        fig, self.ax = plt.subplots(figsize=(10, 10))
+
+        self.ax.set_title('LatticeWorld for the agent to explore')
+        self.ax.set_xlim([-20, 20])
+        self.ax.set_xlabel('x', size=10)
+        self.ax.set_ylim([-15, 15])
+        self.ax.set_ylabel('y', size=10)
+
+        self.draw_world()
+
+    def get_node_by_pos(self, pos):
+        ''' returns the node at the given position '''
+        for node in self.world.nodes():
+            if self.world.nodes[node]['pos'] == pos:
+                return node
+
+    def draw_world(self):
+        ''' draws the world '''
+        nx.draw_networkx_nodes(
+            self.world,
+            pos=nx.get_node_attributes(self.world, 'pos'),
+            ax=self.ax,
+            node_color='grey',
+            node_size=150)
+
+        nx.draw_networkx_edges(
+            self.world,
+            pos=nx.get_node_attributes(self.world, 'pos'),
+            ax=self.ax,
+            edge_color='grey')
+
+        nx.draw_networkx_labels(
+            self.world,
+            pos=nx.get_node_attributes(self.world, 'pos'),
+            ax=self.ax,
+            font_size=10)
+
         plt.axis('on')
         self.ax.tick_params(left=True, bottom=True,
                             labelleft=True, labelbottom=True)
         plt.show()
 
 
-
-class GraphWorldExperiment():
-    def __init__(self):
-        self.structure = [(4, 0), (8, 0), (12, 0), (16, 0), (16, -4), (16, -8), (16, -12), (12, -12), (12, -8), (12, -4), (12, 0), (8, 0),
-                          (5, 6), (0, 6), (-4, 6), (-8, 6), (-12, 6), (-16, 6), (-16, 11)]
-        
-        self.world = nx.generators.navigable_small_world_graph(5)
-        print(self.world.nodes().data())
-        nx.draw(self.world)
-        plt.show()
-
-
 if __name__ == '__main__':
-
-    gw = GraphWorldExperiment()
+    pass
