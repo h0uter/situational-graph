4621528a2e3332a2ea5b1423a16806fb673d2e59
W. Meijer <houterm@gmail.com>
2022-01-12 18:20:47 +0100

    generation of very basic line graph

--- src/data_providers/world_graph_generator.py
+++ src/data_providers/world_graph_generator.py
@@ -1,4 +1,8 @@
 import networkx as nx
+import numpy as np
+
+def get_rand_sign():
+    return np.random.choice([-1, 1])
 
 class GraphGenerator():
     def __init__(self) -> None:
@@ -9,15 +13,17 @@ class GraphGenerator():
 
         for i in range(num_nodes):
             if i == 0:
-                graph.add_node(0, pos=(0, 0))
+                graph.add_node(0, pos=(-20, 0))
             else:
-                # sample 4 meters away from any existing waypoint.
-                # lets just start in 1D
-                
+            
                 old_pos = graph.nodes[i-1]['pos']
-                print(old_pos)
-                new_x = old_pos[0] + 4
-                new_y = old_pos[1] + 0
+                # lets randomly sample between 3 and 5 meters
+                sign1 = get_rand_sign()
+                sign2 = get_rand_sign()
+                sample_dist = np.random.randint(low=3, high=5)
+                sample_dist2 = np.random.randint(low=3, high=5)
+                new_x = old_pos[0] + sample_dist
+                new_y = old_pos[1] + sample_dist2*sign2
                 # new_pos = list(old_pos) + list((4,0))
                 new_pos = (new_x, new_y)
                 
