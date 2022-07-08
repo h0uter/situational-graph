7a172dbce361a0233fa6d57cc4d6042c29ade286
W. Meijer <houterm@gmail.com>
2022-07-08 16:20:08 +0200

    cleanup planning pipeline

--- src/usecases/planning_pipeline.py
+++ src/usecases/planning_pipeline.py
@@ -1,15 +1,12 @@
-from enum import Enum, auto
 import logging
-from abc import ABC, abstractmethod
-from tkinter import E
-from typing import Literal, Optional, Sequence, Tuple
+from typing import Optional, Sequence, Tuple
 
 from src.entities.abstract_agent import AbstractAgent
 from src.entities.tosg import TOSG
 from src.entities.plan import Plan
 from src.utils.config import Config
 from src.entities.dynamic_data.task import Task
-from src.utils.my_types import Edge, EdgeType, Node
+from src.utils.my_types import Edge, Behavior, Node
 from src.usecases.behaviors.goto_behavior import GotoBehavior
 from src.usecases.behaviors.explore_behavior import ExploreBehavior
 from src.entities.static_data.objective import Objective
@@ -39,7 +36,6 @@ class PlanningPipeline:
         self.cfg = cfg
         self.completed = False
         self.plan: Plan = Plan()
-        # self.target_node: Optional[Node] = None
         self._log = logging.getLogger(__name__)
 
         self.initialize(tosg, agent)
@@ -49,21 +45,19 @@ class PlanningPipeline:
         if len(self.plan) >= 1:
             return self.plan[-1][1]
         else:
-            # raise ValueError("edge sequence is empty")
             self.plan.invalidate
             return None
 
-    # HACK: initialization should add this edge to the tasks,
-    # FIXME: this is the start, here I will add the tasks and see them work instantly
     def initialize(self, tosg: TOSG, agent):
         # Add a frontier edge self loop on the start node to ensure a exploration sampling action
-        edge_uuid = tosg.add_my_edge(0, 0, EdgeType.EXPLORE_FT_EDGE)
+        edge_uuid = tosg.add_my_edge(0, 0, Behavior.EXPLORE_FT_EDGE)
 
         tosg.tasks.append(Task(edge_uuid, Objective.EXPLORE))
 
         # spoof the task selection, just select the first one.
         init_explore_task = tosg.tasks[0]
         agent.task = init_explore_task
+
         # obtain the plan which corresponds to this edge.
         init_explore_edge_uuid = init_explore_task.edge_uuid
         init_explore_edge = tosg.get_edge_by_UUID(init_explore_edge_uuid)
@@ -71,23 +65,14 @@ class PlanningPipeline:
         self.plan.edge_sequence = [init_explore_edge]
         self._log.debug(f"target node: {self.target_node}")
 
-        # we also need a target node, what is the role of the plan and the target node both?
-        # self.target_node = 0
-
     # CONTEXT
     def main_loop(self, agent: AbstractAgent, tosg: TOSG) -> bool:
         something_was_done_flag = False
 
-        # target_node = agent.task.get_target_node(krm)
-
-
-        # either we are not removing the task and we keep doing the same thing...
-        # we are not sampling continuously over and over again
-        # we select 0 and we cannot find a path to get there
-
         if self.target_node is None:
-            self._log.debug(f"{agent.name}: No task selected for the agent. Setting one.")
-            # self.target_node = self.target_selection(agent, krm)
+            self._log.debug(
+                f"{agent.name}: No task selected for the agent. Setting one."
+            )
             selected_task = self.task_selection(agent, tosg)
 
             print([task.get_target_node(tosg) for task in tosg.tasks])
@@ -98,9 +83,7 @@ class PlanningPipeline:
         if not self.plan.valid:
             self._log.debug(f"{agent.name}: task : {agent.task}")
             target_node = agent.task.get_target_node(tosg)
-            self._log.debug(
-                f"{agent.name}: No plan. Finding one to {target_node}."
-            )
+            self._log.debug(f"{agent.name}: No plan. Finding one to {target_node}.")
             edge_sequence = self.path_generation(agent, tosg, target_node)
             self.plan.edge_sequence = edge_sequence
 
@@ -131,11 +114,6 @@ class PlanningPipeline:
 
     def destroy_task(self, agent: AbstractAgent, tosg: TOSG):
         self._log.debug(f"{agent.name}:  has a task  {agent.task}")
-        # if agent.task:
-        #     for task in tosg.tasks:
-        #         if task.uuid == agent.task.uuid:
-        #             tosg.tasks.remove(agent.task)
-        #             break
 
         if agent.task:
             if agent.task in tosg.tasks:
@@ -143,11 +121,7 @@ class PlanningPipeline:
                 # agent.task = None
         self._log.debug(f"{agent.name}: destroying task  {agent.task}")
 
-        #     if agent.task.uuid in [task.uuid for task in tosg.tasks]:
-        #         tosg.tasks.remove(agent.task)
-
         agent.clear_task()
-        # self.plan.invalidate()
 
     def check_target_available(self, krm: TOSG) -> bool:
         num_targets = 0
@@ -160,28 +134,20 @@ class PlanningPipeline:
         else:
             return True
 
-    # def clear_target(self) -> None:
-    #     self.target_node = None
-
     def check_target_still_valid(self, krm: TOSG, target_node: Optional[Node]) -> bool:
         if target_node is None:
             return False
         return krm.check_node_exists(target_node)
 
-    # def task_selection(self, agent: AbstractAgent, tosg: TOSG) -> Optional[Node]:
     def task_selection(self, agent: AbstractAgent, tosg: TOSG) -> Optional[Task]:
-
         highest_utility = 0
         optimal_task = None
-
-        tosg.remove_invalid_tasks()
-
-        print(len([task.uuid for task in tosg.tasks]), " tasks uuid")
-        print(len(tosg.tasks), " tasks array")
 
         for task in tosg.tasks:
             print(f"evaluating the utility of tasks: {task}")
             task_target_node = task.get_target_node(tosg)
+
+            # HACK: sometimes we get a task that does not exist anymore.
             if not task_target_node:
                 continue
 
@@ -199,16 +165,7 @@ class PlanningPipeline:
                 highest_utility = utility
                 optimal_task = task
 
-        # return optimal_task.get_edge(tosg)
-        self._log.debug(f"{agent.name}: Selected task: {optimal_task}")
         return optimal_task
-
-        # - [x] obtain list of all task edges
-        # - for each task edge check its utility
-        # - select the task edge with the highest utility.
-        # - return the associated task
-
-        # return target_node
 
     def path_generation(
         self, agent: AbstractAgent, krm: TOSG, target_node: Node
@@ -240,37 +197,24 @@ class PlanningPipeline:
             self._log.warning(
                 f"path_execution()::{agent.name}:: Target is no longer valid."
             )
-            # self.clear_target()
             self.destroy_task(agent, krm)
 
             return []
 
         self._log.debug(f"{agent.name}: action_path: {edge_path}")
-        # print(f"action_path: {action_path}")
-        # current_edge_type = krm.graph.edges[action_path[0], action_path[1]]["type"]
-        # current_edge_type = krm.graph.edges[action_path[0]]["type"]
         current_edge_type = krm.get_type_of_edge_triplet(edge_path[0])
         self._log.debug(f"{agent.name}: current_edge_type: {current_edge_type}")
 
-        if current_edge_type == EdgeType.EXPLORE_FT_EDGE:
-            # action_path = ExploreBehavior(self.cfg).run(agent, krm, action_path)
+        if current_edge_type == Behavior.EXPLORE_FT_EDGE:
             edge_path = ExploreBehavior(self.cfg).execute_pipeline(
                 agent, krm, edge_path
             )
-            # self.clear_target()
             self.destroy_task(agent, krm)
             return []
 
-
-            # either a reset function
-            # or pass and return the action path continuously
-
-        elif current_edge_type == EdgeType.GOTO_WP_EDGE:
-            # action_path = GotoBehavior(self.cfg).run(agent, krm, action_path)
+        elif current_edge_type == Behavior.GOTO_WP_EDGE:
             edge_path = GotoBehavior(self.cfg).execute_pipeline(agent, krm, edge_path)
-            # if len(action_path) < 2:
             if len(edge_path) < 1:
-                # self.clear_target()
                 self.destroy_task(agent, krm)
 
                 return []
