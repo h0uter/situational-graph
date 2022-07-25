01dde44664a7fb814afb874e84d2a596754333a6
W. Meijer <houterm@gmail.com>
2022-07-25 22:58:46 +0200

    added capability to behaviors

--- src/domain/entities/behaviors.py
+++ src/domain/entities/behaviors.py
@@ -1,11 +1,23 @@
 from enum import Enum, auto
 
+class Capabilities(Enum):
+    CAN_ASSESS = auto()
 
 # TODO: add capabilities here.
+# TODO: make BaseBehaviors class to inherit from for specific domains
 class Behaviors(Enum):
-    GOTO = auto()
-    EXPLORE = auto()
-    ASSESS = auto()
+    GOTO = (set(), 1, "Go to a waypoint.")
+    EXPLORE = (set(), 1, "Go to a place and sample new frontiers.")
+    ASSESS = ({Capabilities.CAN_ASSESS}, 2, "Assess a victim.")
+
+    # GOTO = auto()
+    # EXPLORE = auto()
+    # ASSESS = auto()
     # OPEN_DOOR = auto()
     # PLAN_EXTRACTION_WO_EDGE = auto()
     # GUIDE_WP_EDGE = auto()
+
+    def __init__(self, required_capabilities: set[Capabilities], cost: float, description: str):
+        self.required_capabilities = required_capabilities
+        self.cost = cost
+        self.description = description
