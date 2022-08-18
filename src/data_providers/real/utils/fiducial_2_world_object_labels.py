04563c5f60297a01e7548e8482bd9afd5dfb90eb
W. Meijer <houterm@gmail.com>
2022-08-18 13:09:08 +0200

    updated fiducials real world

---
+++ src/data_providers/real/utils/fiducial_2_world_object_labels.py
@@ -0,0 +1,31 @@
+# 202==201
+from src.domain.entities.world_object import WorldObject
+from src.domain.entities.object_types import ObjectTypes
+
+# FIXME: update this with the new world objects
+# WORLD_OBJECT_ID_TO_NAME_MAPPING = {
+#     "201": "victim1",
+#     "202": "victim2",
+#     "208": "fire1",
+#     "207": "fire1",
+#     "211": "victim2",
+#     "212": "victim2",
+# }
+WORLD_OBJECT_ID_TO_NAME_MAPPING = {
+    "204": ObjectTypes.UNKNOWN_VICTIM,
+    "205": ObjectTypes.UNKNOWN_VICTIM,
+    # "208": "fire1",
+    # "207": "fire1",
+    "206": ObjectTypes.UNKNOWN_VICTIM,
+    "207": ObjectTypes.UNKNOWN_VICTIM,
+}
+
+
+def create_wo_from_fiducial(pos: tuple[float, float], fiducial_id: int):
+
+    if str(fiducial_id) in WORLD_OBJECT_ID_TO_NAME_MAPPING:
+        name = WORLD_OBJECT_ID_TO_NAME_MAPPING[str(fiducial_id)]
+        return WorldObject(pos, name)
+    else:
+        # print(f"create_wo_from_fiducial(): unknown fiducial {fiducial_id}")
+        return None
