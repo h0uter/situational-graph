1e476b1ad0f419b339d5ab53c6ef4876df9cceed
W. Meijer <houterm@gmail.com>
2022-12-04 00:39:49 +0100

    before viz refactor

--- src/utils/event.py
+++ src/utils/event.py
@@ -1,19 +1,18 @@
 # OBSERVER PATTERN
 from typing import Callable
 
-
-subscribers: dict[str, list[Callable]] = {}
-
-
-def subscribe(event_type: str, callback_fn: Callable):
-    if event_type not in subscribers:
-        subscribers[event_type] = []
-    subscribers[event_type].append(callback_fn)
+subscriptions: dict[str, list[Callable]] = {}
 
 
-def post_event(event_type: str, event_data):
-    if not (event_type in subscribers):
+def subscribe(topic: str, callback_fn: Callable):
+    if topic not in subscriptions:
+        subscriptions[topic] = []
+    subscriptions[topic].append(callback_fn)
+
+
+def post_event(topic: str, message):
+    if not (topic in subscriptions):
         return
 
-    for callback_fn in subscribers[event_type]:
-        callback_fn(event_data)
+    for callback_fn in subscriptions[topic]:
+        callback_fn(message)
